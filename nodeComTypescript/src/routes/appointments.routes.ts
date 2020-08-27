39509348a6cdff18ef6fe0cbed9aac1c4e710c93
Adriely Barbosa <adriely.nara@gmail.com>
2020-08-27 20:10:18 -0300

    Created the rout separation for post methods - appointmentsRouter

---
+++ nodeComTypescript/src/routes/appointments.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from 'express';
+
+const appointmentsRouter = Router();
+
+const appointments = [];
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+  return response.json({ message: 'Hello world' });
+});
+export default appointmentsRouter;
