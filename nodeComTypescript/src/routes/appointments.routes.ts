c8e881698112382c637b702221d37779dbca333c
Adriely Barbosa <adriely.nara@gmail.com>
2020-08-27 21:13:31 -0300

    Cadastro de agendamentos

--- nodeComTypescript/src/routes/appointments.routes.ts
+++ nodeComTypescript/src/routes/appointments.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { uuid } from 'uuidv4';
 
 const appointmentsRouter = Router();
 
@@ -6,6 +7,15 @@ const appointments = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
-  return response.json({ message: 'Hello world' });
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
 });
+
 export default appointmentsRouter;
