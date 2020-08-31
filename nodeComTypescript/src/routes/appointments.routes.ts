cdb7c8f3a50a0f8699a03d928d7d1a1e1e7159f9
Adriely Barbosa <adriely.nara@gmail.com>
2020-08-31 20:25:27 -0300

    Desestructuring assignment

--- nodeComTypescript/src/routes/appointments.routes.ts
+++ nodeComTypescript/src/routes/appointments.routes.ts
@@ -19,7 +19,10 @@ appointmentsRouter.post('/', (request, response) => {
       .json({ message: 'This appoint is already booked.' });
   }
 
-  const appointment = appointmentsRepository.create(provider, parsedDate);
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
 
   return response.json(appointment);
 });
