9e2b27ced645d750fec8cb10f7be0f73355cb946
Adriely Barbosa <adriely.nara@gmail.com>
2020-09-02 22:08:48 -0300

    Sets the height/width of the circle to the mouse corrdinates.

---
+++ MousePosition/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MousePosition
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MouseMoveHandler(object sender, MouseEventArgs e)
+        {
+            
+            // Get the x and y coordinates of the mouse pointer.
+            System.Windows.Point position = e.GetPosition(this);
+            double pX = position.X;
+            double pY = position.Y;
+
+            // Sets the Height/Width of the circle to the mouse coordinates.
+            ellipse.Width = pX;
+            ellipse.Height = pY;
+            
+
+        }
+    }
+}
